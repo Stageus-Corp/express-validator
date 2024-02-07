6d13496aab1a18b35f3f8f6adf22632f608582cd
jochongs <joch2712@naver.com>
2024-02-07 16:06:46 +0900

    feat: add RequestKey class

---
+++ lib/class/RequestKey.ts
@@ -0,0 +1,17 @@
+import { ArraySchema } from '@stageus/validator/dist/class/schema/ArraySchema';
+import { ValidateSchema } from '@stageus/validator/dist/class/schema/ValidateSchema';
+import { Validator } from '@stageus/validator/dist/class/validate/Validator';
+
+export class RequestKey {
+  constructor(
+    public readonly fieldName: string | null,
+
+    /**
+     * Express Request type method name
+     *
+     * @example body params query
+     */
+    public readonly key: 'body' | 'params' | 'query',
+    public readonly schema: Validator | ArraySchema | ValidateSchema
+  ) {}
+}
