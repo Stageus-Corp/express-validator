b23c6990cb8f829e480925550dc9e652d474b487
jochongs <joch2712@naver.com>
2024-02-09 19:13:10 +0900

    feat: add Params class and params function

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,6 @@
 import { validate } from './util/validate';
 import { body } from './util/body';
+import { query } from './util/query';
+import { params } from './util/params';
 
-export { validate, body };
+export { validate, body, query, params };
