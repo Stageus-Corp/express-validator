f9f1ddccfc7513cd0f9d946e5626f304eb142348
jochongs <joch2712@naver.com>
2024-02-10 00:53:15 +0900

    feat: export @stageus/validator util function

--- lib/index.ts
+++ lib/index.ts
@@ -2,5 +2,6 @@ import { validate } from './util/validate';
 import { body } from './util/body';
 import { query } from './util/query';
 import { params } from './util/params';
+import { object, message, array } from '@stageus/validator';
 
-export { validate, body, query, params };
+export { validate, body, query, params, object, message, array };
