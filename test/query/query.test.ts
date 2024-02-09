e20d9c414728e452831de4d3e0062eaae5b2a81f
jochongs <joch2712@naver.com>
2024-02-09 18:52:23 +0900

    test: add query test case

--- test/query/query.test.ts
+++ test/query/query.test.ts
@@ -98,4 +98,81 @@ describe('body test ( Success )', () => {
       page: 1,
     });
   });
+
+  test('6 - query test', () => {
+    const req: any = {
+      query: {
+        name: {
+          list: ['1', '2', '3'],
+        },
+      },
+    };
+    const res: any = {};
+    const next = jest.fn();
+
+    validate([
+      query({
+        name: {
+          list: array(message().isNumber()),
+        },
+      }),
+    ])(req, res, next);
+
+    expect(next).toHaveBeenCalledWith();
+    expect(req.query).toStrictEqual({
+      name: {
+        list: [1, 2, 3],
+      },
+    });
+  });
+});
+
+describe('body test ( Fail )', () => {
+  test('1 - query test', () => {
+    const req: any = {
+      query: {
+        page: '-1',
+      },
+    };
+    const res: any = {};
+    const next = jest.fn();
+
+    validate([
+      query({
+        page: message('invalid page')
+          .default(1)
+          .isNumber()
+          .isInt()
+          .range({ min: 1 }),
+      }),
+    ])(req, res, next);
+
+    expect(next).toHaveBeenCalledWith(
+      expect.objectContaining({
+        status: 400,
+      })
+    );
+  });
+
+  test('1 - query test', () => {
+    const req: any = {
+      query: {
+        page: null,
+      },
+    };
+    const res: any = {};
+    const next = jest.fn();
+
+    validate([
+      query({
+        page: message('invalid page').isNumber().isInt().range({ min: 1 }),
+      }),
+    ])(req, res, next);
+
+    expect(next).toHaveBeenCalledWith(
+      expect.objectContaining({
+        status: 400,
+      })
+    );
+  });
 });
